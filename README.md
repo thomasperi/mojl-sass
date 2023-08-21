ceff10489097403e29365aa3ba5d9b0287d3ed32
Thomas Peri <tjperi@gmail.com>
2023-08-21 03:46:11 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# mojl-sass
+
+A simple adapter for using sass with mojl
+
+```javascript
+new Mojl({
+  cssTranspilerAdapter: 'mojl-sass'
+});
+```
---
+++ index.js
@@ -0,0 +1,42 @@
+const sass = require('sass');
+const fs = require('fs');
+const path = require('path').posix;
+
+module.exports.inputTypes = ['scss', 'css'];
+
+module.exports.run = async ({entryPath, isDev, outputPath, sourcePaths}) => {
+	const entryFile = sourcePaths.map(source => `@use ${JSON.stringify(source)};`).join('\n');
+	const options = isDev ? {
+		sourceMap: true,
+	} : {
+		style: 'compressed',
+	};
+
+	await Promise.all([
+		fs.promises.mkdir(path.dirname(entryPath), {recursive: true}),
+		fs.promises.mkdir(path.dirname(outputPath), {recursive: true}),
+	]);
+	
+	await fs.promises.writeFile(entryPath, entryFile, 'utf8');
+
+	const result = sass.compile(entryPath, options);
+	let css = result.css;
+	
+	if (result.sourceMap) {
+		// Relativize source paths to the output file.
+		result.sourceMap.sources = result.sourceMap.sources.map(source => {
+			source = source.replace(/^file:\/\//, '');
+			source = path.relative(path.dirname(outputPath), source);
+			return source;
+		});
+
+		// Embed the source map in the css file.
+		// Thanks to: https://github.com/sass/dart-sass/issues/1594#issuecomment-1013208452
+		const sm = JSON.stringify(result.sourceMap);
+		const smBase64 = (Buffer.from(sm, 'utf8') || '').toString('base64');
+		const smComment = '/*# sourceMappingURL=data:application/json;charset=utf-8;base64,' + smBase64 + ' */';
+		css += '\n\n' + smComment;
+	}
+
+	await fs.promises.writeFile(outputPath, css, 'utf8');
+};
